e26fc2dd56c5db99a48e52434072e8e281226e4e
JPG200 <giraldoholguinjuanpablo@gmail.com>
2025-04-02 07:31:41 -0500

    se actualiza el welcome.php y el msg

--- assets/js/Usuario.js
+++ assets/js/Usuario.js
@@ -13,18 +13,15 @@ function ValidarUsuario(){
         type:  'post',
         beforeSend: function () {},
         success: function (response) {
-            console.log(response);
             if(response == "success"){
                 console.log(response);
                 location.href = "/pages/welcome.php";
             } else if(response == "not found"){
                 console.log(response);
-               msg ='<div class="alert alert-danger mb-2" role="alert"><strong>Usuario o Contraseña incorrecta </strong>'+
-                'Las Credenciales son incorrectas. Por favor verifique.</div>';
+               msg ='<div class="alert alert-danger mb-2" role="alert"><strong>Usuario o Contraseña incorrecta </strong>Las Credenciales son incorrectas. Por favor verifique.</div>;'
             } else if(response == "requerido"){
                 console.log(response);
-               msg ='<div class="alert alert-danger mb-2" role="alert"><strong>Usuario no existe </strong>'+
-                'Por favor registrese.</div>';
+               msg ='<div class="alert alert-danger mb-2" role="alert"><strong>Usuario no existe </strong> Por favor registrese.</div>;'
             }
             $('#status_logic').html(msg);
         }
--- pages/layouts/navbar.php
+++ pages/layouts/navbar.php
@@ -3,7 +3,7 @@
         <div class="navbar-header">
           <ul class="nav navbar-nav">
             <li class="nav-item mobile-menu hidden-md-up float-xs-left"><a class="nav-link nav-menu-main menu-toggle hidden-xs"><i class="icon-menu5 font-large-1"></i></a></li>
-            <li class="nav-item"><a href="index.html" class="navbar-brand nav-link"><img alt="branding logo" src="../app-assets/images/logo/robust-logo-light.png" data-expand="../app-assets/images/logo/robust-logo-light.png" data-collapse="../app-assets/images/logo/robust-logo-small.png" class="brand-logo"></a></li>
+            <li class="nav-item"><a href="index.html" class="navbar-brand nav-link"><img alt="branding logo" src="../app-assests/images/logo/robust-logo-light.png" data-expand="../app-assests/images/logo/robust-logo-light.png" data-collapse="../app-assests/images/logo/robust-logo-small.png" class="brand-logo"></a></li>
             <li class="nav-item hidden-md-up float-xs-right"><a data-toggle="collapse" data-target="#navbar-mobile" class="nav-link open-navbar-container"><i class="icon-ellipsis pe-2x icon-icon-rotate-right-right"></i></a></li>
           </ul>
         </div>
@@ -16,7 +16,7 @@
             <ul class="nav navbar-nav float-xs-right">
               <li class="dropdown dropdown-notification nav-item"><a href="#" data-toggle="dropdown" class="nav-link nav-link-label"><i class="ficon icon-bell4"></i><span class="tag tag-pill tag-default tag-danger tag-default tag-up">5</span></a></li>
               <li class="dropdown dropdown-notification nav-item"><a href="#" data-toggle="dropdown" class="nav-link nav-link-label"><i class="ficon icon-mail6"></i><span class="tag tag-pill tag-default tag-info tag-default tag-up">8</span></a></li>
-              <li class="dropdown dropdown-user nav-item"><a href="#" data-toggle="dropdown" class="dropdown-toggle nav-link dropdown-user-link"><span class="avatar avatar-online"><img src="../app-assets/images/portrait/small/avatar-s-1.png" alt="avatar"><i></i></span><span class="user-name"></span></a>
+              <li class="dropdown dropdown-user nav-item"><a href="#" data-toggle="dropdown" class="dropdown-toggle nav-link dropdown-user-link"><span class="avatar avatar-online"><img src="../app-assests/images/portrait/small/avatar-s-1.png" alt="avatar"><i></i></span><span class="user-name"></span></a>
                 <div class="dropdown-menu dropdown-menu-right">
                     <a href="#" class="dropdown-item"><i class="icon-head"></i> Editar perfil</a>
                   <div class="dropdown-divider"></div><a href="#" class="dropdown-item"><i class="icon-power3"></i>Cerrar sesión</a>
