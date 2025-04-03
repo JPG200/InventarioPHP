58a9e6dd6b3c3491c8b902174f9332d552babe66
JPG200 <giraldoholguinjuanpablo@gmail.com>
2025-04-03 16:58:32 -0500

    Revert "Se crea ActualizarEquipos"
    This reverts commit fc0f380e6f1b758b962e84ed6d90d1da5d3ab403.

--- model/Equipos.php
+++ model/Equipos.php
@@ -2,84 +2,26 @@
 require "../config/Conexion.php";
 
 class Equipos{
-    public $cnx;
+public $cnx;
 
-    function __construct(){
-        $this->cnx = Conexion::ConectarBD();
-    }
+function __construct(){
+    $this->cnx = Conexion::ConectarBD();
+}
 
-    function ListarEquipos(){
-        $query="SELECT * from tbequipos;";
+function ListarEquipos(){
+    $query="SELECT tbeq.id_Equip,tbeq.placa,tbeq.serial,tbreq.descripcion,tbreq.observaciones,tbreq.accesorios,tbreq.empresa, tbreq.fecha_creacion,tbreq.estado
+     FROM tbequipos tbeq INNER JOIN tbregequip tbreq ON tbeq.id_Equip=tbreq.id_Equip WHERE tbeq.estado=1;";
 
-        $result = $this->cnx->prepare($query);
-
-        if($result->execute()){
-                if($result->rowCount()>0){
-                    while($fila = $result->fetch(PDO::FETCH_ASSOC)){
-                        $datos[]=$fila;
-                    }
-                    return $datos;
-                }
-        }
-        return false;
-    }
-
-    function RegistrarEquipos($placa,$serial){
-
-            $estado = 1; //Activo por defecto
-            $fecha_creacion=date( 'Y-m-d H:i:s',time()); //Fecha creacion del equipo
-            $query="INSERT INTO tbequipos(placa,serial,fecha_creacion,estado) VALUES(?,?,?,?);";
-
-            $result = $this->cnx->prepare($query);
-            $result->bindParam(1,$placa);
-            $result->bindParam(2,$serial);
-            $result->bindParam(3,$fecha_creacion);
-            $result->bindParam(4,$estado);
-
-            if($result->execute()){
-                return true;
-            }else{
-                return false;
-            }
-        
-    }
-    function buscarEquipo($id){
-        $query="SELECT * from tbequipos where id_Equip = ?;";
-        $result = $this->cnx->prepare($query);
-        $result->bindParam(1,$id);
-        if($result->execute()){
+    $result = $this->cnx->prepare($query);
+    if($result->execute()){
             if($result->rowCount()>0){
-                return $result->fetch(PDO::FETCH_ASSOC);
+                while($fila = $result->fetch(PDO::FETCH_ASSOC)){
+                    $datos[]=$fila;
+                }
+                return $datos;
             }
-            return false;
-        }
-        return false;
     }
-
-    function ActualizarEquipos($id,$placa,$serial){
-        $query="UPDATE tbequipos set placa = ?, serial = ? where id_Equip = ?;";
-        $result = $this->cnx->prepare($query);
-        $result->bindParam(1,$placa);
-        $result->bindParam(2,$serial);
-        $result->bindParam(3,$id);
-
-        if($result->execute()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    function Verificar($placa){
-        $query="SELECT * from tbequipos where placa = ? and estado = 1;";
-        $result = $this->cnx->prepare($query);
-        $result->bindParam(1,$placa);
-        if($result->execute()){
-            if($result->rowCount()<=0){
-                return true; //No existe el equipo o esta inactivo
-            }
-            return false; //El equipo ya existe o esta activo
-        }
-    }
+    return false;
+}
 }
 ?>
