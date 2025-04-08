9e4002816da64af9b5a50f86354e92e9e59790da
JPG200 <giraldoholguinjuanpablo@gmail.com>
2025-04-08 11:10:46 -0500

    se crea modal update reg Equipo

--- assets/js/regEquipos.js
+++ assets/js/regEquipos.js
@@ -39,9 +39,8 @@ function cerrarModal(){
     $('#createregEquipo').modal('hide'); // Cerrar el modal después de registrar
 }
 
-function BuscarEquipo(){
+function BuscarEquipo(placa,op){
     // Obtener los valores de los campos de entrada
-    placa = $("#txtplacacrear").val();
     parametros = {
         "placa": placa,
         },
@@ -55,6 +54,7 @@ function BuscarEquipo(){
             console.log(response); // Para depuración
             if(response && response.length > 0){
                 data = $.parseJSON(response);
+                if(op=="registrar"){
                         $("txtplacacrear").val(placa);
                         $('#txtserialcrear').val(data[0]['serial']);
                         if(data[0]['descripcion']!=null && data[0]['descripcion']!=""){
@@ -78,6 +78,28 @@ function BuscarEquipo(){
                             $('#txtempresacrear').val("");
                             toastr.success("El equipo no se ha registrado.", "Registre los datos del equipo"); // Mostrar mensaje de éxito
                         }
+                    }else if(op=="editar"){
+                        $('#txtplacaupdate').val(placa);     
+                        $('#txtserialupdate').val(data[0]['serial']);
+                        if(data[0]['descripcion']!=null && data[0]['descripcion']!=""){
+                            $('#txtdescripcionupdate').val(data[0]['descripcion']);
+                            $('#txtobservacionesupdate').val(data[0]['observaciones']);
+                            $('#txtaccesoriosupdate').val(data[0]['accesorios']);
+
+                            var nombreEmpresaEncontrada = data[0]['empresa'];
+
+                            // Buscar la opción en el select que coincida con el nombre de la empresa
+                            $('#txtempresaupdate option').each(function() {
+                                if ($(this).text().toUpperCase() === nombreEmpresaEncontrada.toUpperCase()) {
+                                    $('#txtempresaupdate').val($(this).val()); // Seleccionar la opción por su valor
+                                    return false; // Romper el bucle each una vez encontrada la coincidencia
+                                }
+                            });
+                            toastr.success("Actualice el registro del equipo.", "Actualice el Registro"); // Mostrar mensaje de éxito      
+                        }                   
+                    }else if(op=="eliminar"){
+
+                    }
 
             }else{
             toastr.success("Error al buscar el equipo", "ERROR"); // Mostrar mensaje de éxito
@@ -125,6 +147,7 @@ function LlenarSelectorEmpresa(){
                     options += '<option value="' + data[i]['id_Empresa'] + '">' + data[i]['Empresa'] + '</option>';
                 }
                 $('#txtempresacrear').html(options);
+                $('#txtempresaupdate').html(options);
             }else{
             toastr.error("Error al cargar las empresas", "ERROR"); // Mostrar mensaje de éxito
             }
@@ -180,12 +203,12 @@ function RegistrarEquipo(){
 
 function EditarEquipo(){
     // Obtener los valores de los campos de entrada
-    placa = $("#txtplacacrear").val();
-    serial = $("#txtserialcrear").val();
-    descripcion = $("#txtdescripcioncrear").val();
-    observaciones = $("#txtobservacionescrear").val();
-    accesorios = $("#txtaccesorioscrear").val();
-    empresa = $("#txtempresacrear").val();
+    placa = $("#txtplacaupdate").val();
+    serial = $("#txtserialupdate").val();
+    descripcion = $("#txtdescripcionupdate").val();
+    observaciones = $("#txtobservacionesupdate").val();
+    accesorios = $("#txtaccesoriosupdate").val();
+    empresa = $("#txtempresaupdate").val();
 
     parametros = {
         "placa": placa,
@@ -206,11 +229,12 @@ function EditarEquipo(){
             if(response.length > 0){
                 data = $.parseJSON(response);
                 if(data.length > 0){
-                        $('#txtserialcrear').val(data[0]['serial']);
-                        $('#txtdescripcioncrear').val(data[0]['descripcion']);
-                        $('#txtobservacionescrear').val(data[0]['observaciones']);
-                        $('#txtaccesorioscrear').val(data[0]['accesorios']);
-                        $('#txtempresacrear').val(data[0]['empresa']);
+                        $('#txtplacaupdate').val(placa);
+                        $('#txtserialupdate').val(data[0]['serial']);
+                        $('#txtdescripcionupdate').val(data[0]['descripcion']);
+                        $('#txtobservacionesupdate').val(data[0]['observaciones']);
+                        $('#txtaccesoriosupdate').val(data[0]['accesorios']);
+                        $('#txtempresaupdate').val(data[0]['empresa']);
                         toastr.success("El Equipo ya se encuentra registrado.", "Equipo Registrado"); // Mostrar mensaje de éxito      
                 }
             }else{
--- controller/regEquiposController.php
+++ controller/regEquiposController.php
@@ -4,198 +4,200 @@ require "../model/regEquipos.php";
 $cat = new regEquipos();
 
 switch($_REQUEST["operador"]){
-case "listar_regEquipos":
-$datos = $cat->ListarregEquipos();
-if(is_array(value: $datos)){
-for($i=0;$i<count($datos);$i++){
-$list[]=array(
-"Numero de Registro"=>$datos[$i]["id_Reg"],
-"Placa"=> $datos[$i]['placa'],
-"Serial"=> $datos[$i]['serial'],
-"Descripcion"=> $datos[$i]['descripcion'],
-"Observaciones"=> $datos[$i]['observaciones'],
-"Accesorios"=> $datos[$i]['accesorios'],
-"Empresa"=> $datos[$i]['Empresa'],
-"Fecha de Ingreso"=> $datos[$i]['fecha_creacion'],
-"Estado"=> $datos[$i]['estado']==1?'<div class="tag tag-success">Activo</div>':
-                                    '<div class="tag tag-danger">Inactivo</div>',
-"op"=> '<div class="btn-group">
-<button class="btn btn-info dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-  <i class="icon-gear"></i>
-  </button>
-  <div class="dropdown-menu">
-    <a class="dropdown-item" href="#"><i class="icon-pencil"></i> Editar</a>
-    <a class="dropdown-item" href="#"><i class="icon-trash"></i> Eliminar</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="#"><i class="icon-eye"></i> Ver</a>
-</div>'
-);
-}
-$resultador = array(
-    "sEcho"=>1,
-    "iTotalRecords"=>count($list),
-    "iTotalDisplayRecords"=>count($list),
-    "aaData"=>$list
-);
-} else{
-    $list[]=array(
-        "Numero de Registro"=>"ERROR",
-        "Placa"=> "ERROR",
-        "Serial"=> "ERROR",
-        "Descripcion"=> "ERROR",
-        "Observaciones"=> "ERROR",
-        "Accesorios"=> "ERROR",
-        "Empresa"=> "ERROR",
-        "Fecha de Ingreso"=> "ERROR",
-        "Estado"=> "ERROR",
-        "op"=> "ERROR"
-        );
-        $resultador = array(
-            "sEcho"=>1,
-            "iTotalRecords"=>count($list),
-            "iTotalDisplayRecords"=>count($list),
-            "aaData"=>$list
-        );
- }
-
-echo json_encode($resultador);
-
-break;
-
-case "buscarEquipo":
-
-if(isset($_POST["placa"]) && !empty($_POST["placa"])){
-        $data = $cat->buscarEquipo($_POST["placa"]);
-            if($data && $data["descripcion"]!=null){
-                $list[] = array(
-                    "placa"=>$data["placa"],
-                    "serial"=>$data["serial"],
-                    "descripcion"=>$data["descripcion"],
-                    "observaciones"=>$data["observaciones"],
-                    "accesorios"=>$data["accesorios"],
-                    "empresa"=>$data["Empresa"]
+    case "listar_regEquipos":
+        $datos = $cat->ListarregEquipos();
+        if(is_array(value: $datos)){
+            for($i=0;$i<count($datos);$i++){
+                $list[]=array(
+                    "Numero de Registro"=>$datos[$i]["id_Reg"],
+                    "Placa"=> $datos[$i]['placa'],
+                    "Serial"=> $datos[$i]['serial'],
+                    "Descripcion"=> $datos[$i]['descripcion'],
+                    "Observaciones"=> $datos[$i]['observaciones'],
+                    "Accesorios"=> $datos[$i]['accesorios'],
+                    "Empresa"=> $datos[$i]['Empresa'],
+                    "Fecha de Ingreso"=> $datos[$i]['fecha_creacion'],
+                    "Estado"=> $datos[$i]['estado']==1?'<div class="tag tag-success">Activo</div>':
+                                                        '<div class="tag tag-danger">Inactivo</div>',
+                    "op"=> '<div class="btn-group">
+                    <button class="btn btn-info dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <i class="icon-gear"></i>
+                    </button>
+                    <div class="dropdown-menu">
+                            <a class="dropdown-item" data-toggle="modal" data-target="#updateregEquipo"
+                            onclick="BuscarEquipo('.$datos[$i]['placa'].",'editar'".');">
+                            <i class="icon-pencil"></i> Editar</a>
+                        <a class="dropdown-item" href="#"><i class="icon-trash"></i> Eliminar</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"><i class="icon-eye"></i> Ver</a>
+                    </div>'
                 );
-                echo json_encode($list);
-            }else{
-                $data = $cat->buscarSerial($_POST["placa"]);
-                $list[] = array(
-                    "placa"=>$data["placa"],
-                    "serial"=>$data["serial"],
-                    "descripcion"=>"",
-                    "observaciones"=>"",
-                    "accesorios"=>"",
-                    "empresa"=>""
-                );
-                echo json_encode($list);
             }
-            $response = array(
-                "error"=>"error"
+            $resultador = array(
+                "sEcho"=>1,
+                "iTotalRecords"=>count($list),
+                "iTotalDisplayRecords"=>count($list),
+                "aaData"=>$list
             );
-        }
-        break;
-        case "confirmarInformacion":
-
-        break;
-
-        case"LlenarSelectEmpresas":
-            $data = $cat->LlenarSelectEmpresas();
-            if($data){
-                for($i=0;$i<count($data);$i++){
-                    $list[]=array(
-                        "id_Empresa"=>$data[$i]["id_Empresa"],
-                        "Empresa"=>$data[$i]['Empresa']
+            } else{
+                $list[]=array(
+                    "Numero de Registro"=>"ERROR",
+                    "Placa"=> "ERROR",
+                    "Serial"=> "ERROR",
+                    "Descripcion"=> "ERROR",
+                    "Observaciones"=> "ERROR",
+                    "Accesorios"=> "ERROR",
+                    "Empresa"=> "ERROR",
+                    "Fecha de Ingreso"=> "ERROR",
+                    "Estado"=> "ERROR",
+                    "op"=> "ERROR"
                     );
+                    $resultador = array(
+                        "sEcho"=>1,
+                        "iTotalRecords"=>count($list),
+                        "iTotalDisplayRecords"=>count($list),
+                        "aaData"=>$list
+                    );
+            }
+
+        echo json_encode($resultador);
+
+    break;
+
+    case "buscarEquipo":
+
+    if(isset($_POST["placa"]) && !empty($_POST["placa"])){
+            $data = $cat->buscarEquipo($_POST["placa"]);
+                if($data && $data["descripcion"]!=null){
+                    $list[] = array(
+                        "placa"=>$data["placa"],
+                        "serial"=>$data["serial"],
+                        "descripcion"=>$data["descripcion"],
+                        "observaciones"=>$data["observaciones"],
+                        "accesorios"=>$data["accesorios"],
+                        "empresa"=>$data["Empresa"]
+                    );
+                    echo json_encode($list);
+                }else{
+                    $data = $cat->buscarSerial($_POST["placa"]);
+                    $list[] = array(
+                        "placa"=>$data["placa"],
+                        "serial"=>$data["serial"],
+                        "descripcion"=>"",
+                        "observaciones"=>"",
+                        "accesorios"=>"",
+                        "empresa"=>""
+                    );
+                    echo json_encode($list);
                 }
-                echo json_encode($list);
-            }else{
                 $response = array(
                     "error"=>"error"
                 );
             }
             break;
-case "registrarEquipo":
-    if(isset($_POST["placa"]) && !empty($_POST["placa"]) && $_POST["descripcion"]
-    && !empty($_POST["descripcion"]) && $_POST["observaciones"] && !empty($_POST["observaciones"]) 
-    && $_POST["accesorios"] && !empty($_POST["accesorios"]) && $_POST["empresa"] && !empty($_POST["empresa"])){
-        $placa=$_POST["placa"];
-    if($cat->Verificar($placa)){
-        if($cat->RegistrarRegistroEquipo($_POST["placa"],$_POST["descripcion"],$_POST["observaciones"],
-        $_POST["accesorios"],$_POST["empresa"])){
-            $response ="sucess";
+            case "confirmarInformacion":
 
+            break;
+
+            case"LlenarSelectEmpresas":
+                $data = $cat->LlenarSelectEmpresas();
+                if($data){
+                    for($i=0;$i<count($data);$i++){
+                        $list[]=array(
+                            "id_Empresa"=>$data[$i]["id_Empresa"],
+                            "Empresa"=>$data[$i]['Empresa']
+                        );
+                    }
+                    echo json_encode($list);
+                }else{
+                    $response = array(
+                        "error"=>"error"
+                    );
+                }
+                break;
+    case "registrarEquipo":
+        if(isset($_POST["placa"]) && !empty($_POST["placa"]) && $_POST["descripcion"]
+        && !empty($_POST["descripcion"]) && $_POST["observaciones"] && !empty($_POST["observaciones"]) 
+        && $_POST["accesorios"] && !empty($_POST["accesorios"]) && $_POST["empresa"] && !empty($_POST["empresa"])){
+            $placa=$_POST["placa"];
+        if($cat->Verificar($placa)){
+            if($cat->RegistrarRegistroEquipo($_POST["placa"],$_POST["descripcion"],$_POST["observaciones"],
+            $_POST["accesorios"],$_POST["empresa"])){
+                $response ="sucess";
+
+            }else{
+                $response ="required";
+            }
         }else{
-            $response ="required";
+            $response ="registered";
         }
-    }else{
-        $response ="registered";
-    }
-    echo $response;
-    }
-    break;
-    
-/*
-case "actualizarEquipo":
-    if(isset($_POST["placa"]) && !empty($_POST["placa"])){
-        $data = $cat->actualizarEquipo($_POST["id"],$_POST["placa"],$_POST["serial"],$_POST["descripcion"],$_POST["observaciones"],$_POST["accesorios"],$_POST["empresa"]);
-        if($data){
-            $response = array(
-                "success"=>"success"
-            );
-        }else{
-            $response = array(
-                "error"=>"error"
-            );
+        echo $response;
         }
-    }else{
-        $response = array(
-            "error"=>"error"
-        );
-    }
-    echo json_encode($response);
-    break;
-
-
-case "eliminarEquipo":
-    if(isset($_POST["id"]) && !empty($_POST["id"])){
-        $data = $cat->eliminarEquipo($_POST["id"]);
-        if($data){
-            $response = array(
-                "success"=>"success"
-            );
-        }else{
-            $response = array(
-                "error"=>"error"
-            );
-        }
-    }else{
-        $response = array(
-            "error"=>"error"
-        );
-    }
-    echo json_encode($response);
-    break;
-
-
-case "activarEquipo":
-    if(isset($_POST["id"]) && !empty($_POST["id"])){
-        $data = $cat->activarEquipo($_POST["id"]);
-        if($data){
-            $response = array(
-                "success"=>"success"
-            );
+        break;
+        
+    /*
+    case "actualizarEquipo":
+        if(isset($_POST["placa"]) && !empty($_POST["placa"])){
+            $data = $cat->actualizarEquipo($_POST["id"],$_POST["placa"],$_POST["serial"],$_POST["descripcion"],$_POST["observaciones"],$_POST["accesorios"],$_POST["empresa"]);
+            if($data){
+                $response = array(
+                    "success"=>"success"
+                );
+            }else{
+                $response = array(
+                    "error"=>"error"
+                );
+            }
         }else{
             $response = array(
                 "error"=>"error"
             );
         }
-    }else{
-        $response = array(
-            "error"=>"error"
-        );
-    }
-    echo json_encode($response);
-    break;
-*/
+        echo json_encode($response);
+        break;
+
+
+    case "eliminarEquipo":
+        if(isset($_POST["id"]) && !empty($_POST["id"])){
+            $data = $cat->eliminarEquipo($_POST["id"]);
+            if($data){
+                $response = array(
+                    "success"=>"success"
+                );
+            }else{
+                $response = array(
+                    "error"=>"error"
+                );
+            }
+        }else{
+            $response = array(
+                "error"=>"error"
+            );
+        }
+        echo json_encode($response);
+        break;
+
+
+    case "activarEquipo":
+        if(isset($_POST["id"]) && !empty($_POST["id"])){
+            $data = $cat->activarEquipo($_POST["id"]);
+            if($data){
+                $response = array(
+                    "success"=>"success"
+                );
+            }else{
+                $response = array(
+                    "error"=>"error"
+                );
+            }
+        }else{
+            $response = array(
+                "error"=>"error"
+            );
+        }
+        echo json_encode($response);
+        break;
+    */
 }
 
 
